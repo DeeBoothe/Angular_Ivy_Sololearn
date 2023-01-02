7d4fc1fa609177391d75ee66e768c9a8d3625ba4
DeeBoothe <68389970+DeeBoothe@users.noreply.github.com>
2023-01-02 16:13:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular_Ivy_Sololearn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-7cakf9)
---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, VERSION } from '@angular/core';
+import {Car} from './car';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  title = 'Star Wars';
+
+  colors:string[] =['red', 'blue', 'yellow'];
+
+  speak(){
+    const sentence = this.name + this.title;
+    return sentence;
+  }
+
+  subaru: Car ={make:"Subaru", model: "Outback", miles: 58262};
+  honda: Car ={make:'Honda', model: "Accord", miles:34501};
+  bmw: Car ={make:"BMW", model:"X3", miles: 4400};
+
+  cars:Car[]=[this.subaru, this.honda, this.bmw];
+}
